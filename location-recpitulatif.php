cf0dd57f816d9ea5d1cbf493866ba2651c05691d
RimeLahkakCY <166617045+RimeLahkakCY@users.noreply.github.com>
2025-03-23 21:44:52 +0100

    Update location-recpitulatif.php
    Phase 2

--- location-recpitulatif.php
+++ location-recpitulatif.php
@@ -4,7 +4,7 @@ session_start();
 $i= isset($_GET['i']) ? (int) $_GET['i'] : 0;
 ?>
 <!DOCTYPE html>
-<html lang="fr">
+<html lang="fr"> 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
