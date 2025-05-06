44c70b0d9841ad1552774783726dfd8d5bfda6e1
TNorah <168777016+TNorah@users.noreply.github.com>
2025-05-06 22:15:36 +0200

    Update main.php

--- php/main.php
+++ php/main.php
@@ -1,30 +1,25 @@
 <?php 
     session_start();
-	$style="main";
-	setcookie("style",$style,time()+(365*24*36000),"/");
-	if(isset($_COOKIE['style'])){
-		$style=$_COOKIE['style'];
-	}
+	
+	if(!isset($_COOKIE['style'])){
+		setcookie("style", "main", time() + 360*60, "/");
+		$style = "main";
+	}else{
+		$style = $_COOKIE['style'];
+	}	
 ?>
 
 <html>
 
 <head>
     <meta charset="UTF-8">
-    <?php if(isset($_COOKIE['style'])):?>
-	<?php if($_COOKIE['style']=='dark'):?>
-		<link class="aaa" href="../css/dark.css" rel="stylesheet">
-	<?php else :?>
-		<link class="aaa" href="../css/main.css" rel="stylesheet">
-    <?php endif; ?>
-	<?php endif; ?>
+    <link rel="stylesheet" href="../css/<?php echo $style; ?>.css">
     <link rel="icon" type="image/x-icon" href="../img/logo.png">
     <link
         href="https://fonts.googleapis.com/css2?family=Arvo:ital,wght@0,400;0,700;1,400;1,700&family=Calistoga&family=Didact+Gothic&family=Funnel+Sans:ital,wght@0,300..800;1,300..800&display=swap"
         rel="stylesheet">
     <title>Projet web</title>
     <script type="text/javascript" src="../test.js"></script>
-
 </head>
 
 <body>
@@ -78,8 +73,8 @@
 
             <div style="display: flex; align-items: center; margin: 15px;">
             	<?php if (isset($_COOKIE['style'])):?>
-		<img class="mode" id="mode" onclick="color();" height="25px" src="../img/dark_mode.png"/>
-	    <?php endif; ?>
+				<img class="mode" id="mode" onclick="color();" height="25px" src="../img/dark_mode.png"/>
+	    		<?php endif; ?>
             </div>
 
         </div>
@@ -105,7 +100,7 @@
 
         <div class="content mainB">
 
-		    <h1 class="selection_titre">Nos sélections du moment : </h1>
+		    <h1 style="color:blue">Nos sélections du moment : </h1>
 			
                 <div class="voyages">
                 <br/>
