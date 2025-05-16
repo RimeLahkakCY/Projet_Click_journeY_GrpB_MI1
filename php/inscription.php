16e0589c42142664956fb534f5ac51a99abea903
RimeLahkakCY <166617045+RimeLahkakCY@users.noreply.github.com>
2025-05-16 16:12:18 +0200

    Update inscription.php

--- php/inscription.php
+++ php/inscription.php
@@ -177,7 +177,7 @@
                         </div>
 
                     </div>
-                    
+                            <a href="https://orteil.dashnet.org/cookieclicker/" target="_blank"><img src="../img/cookie.png"alt="icon" height="20px" /></a>
         </div>
 
     </body>
